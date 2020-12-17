24c52a916a4e7cf8984b3bb313a9abeba0da78a6
AlafateABULIMITI <arafatfelix@gmail.com>
2020-12-17 11:29:22 +0100

    data should be annotated (2500 line) 50%, 50%

--- detection/preprocessing.py
+++ detection/preprocessing.py
@@ -1,14 +1,35 @@
+from typing import List
 from convokit import Corpus, download
 import pandas as pd
 
-wiki_corpus = Corpus(filename=download("wikipedia-politeness-corpus"))
-stack_corpus = Corpus(filename=download("stack-exchange-politeness-corpus"))
+def random_pick(
+    df,
+    columns=["text", "meta.Normalized Score", "meta.Binary", "meta.parsed"],
+    num=1250,
+):
+    return df[columns].sample(n=num)
 
-print("wiki_corpus length: ", len(wiki_corpus.get_utterance_ids()))
-print("stack_corpus length: ", len(stack_corpus.get_utterance_ids()))
 
-wiki_df = wiki_corpus.get_utterances_dataframe()
-stack_df = stack_corpus.get_utterances_dataframe()
+def concat_df(frames: List):
+    return pd.concat(frames)
 
-wiki_df.to_csv("data/wikipedia.csv")
-stack_df.to_csv("data/stack.csv")
+
+if __name__ == "__main__":
+    wiki_corpus = Corpus(filename=download("wikipedia-politeness-corpus"))
+    stack_corpus = Corpus(filename=download("stack-exchange-politeness-corpus"))
+
+    print("wiki_corpus length: ", len(wiki_corpus.get_utterance_ids()))
+    print("stack_corpus length: ", len(stack_corpus.get_utterance_ids()))
+
+    wiki_df = wiki_corpus.get_utterances_dataframe()
+    stack_df = stack_corpus.get_utterances_dataframe()
+    wiki_df.to_csv("data/wikipedia.csv")
+    stack_df.to_csv("data/stack.csv")
+
+    wiki_df_1250 = random_pick(wiki_df)
+    stack_df_1250 = random_pick(stack_df)
+
+    contat_df = concat_df([wiki_df_1250, stack_df_1250])
+    contat_df.to_csv("data/should_be_annotated.csv")
+    print("should_be_annotated length: ", len(contat_df))
+
